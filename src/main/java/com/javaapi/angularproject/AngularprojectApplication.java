bcf981ef22b44c35aadfd814ddc972e37b827ce0
bryant pang <bryant.pang@fdmgroup.com>
2024-06-21 14:56:28 +0800

    Initial commit

---
+++ src/main/java/com/javaapi/angularproject/AngularprojectApplication.java
@@ -0,0 +1,13 @@
+package com.javaapi.angularproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularprojectApplication.class, args);
+	}
+
+}
